a1306d111a7f8d7c5a3daf6eb3a84bd86a39df51
Austin Moore <austinymoore@gmail.com>
2016-02-22 14:09:09 -0500

    Start

---
+++ src/MVAMVC/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using MVAMVC.Models;
+using Microsoft.Extensions.OptionsModel;
+
+namespace MVAMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController( IOptions<MyOptions> o )
+        {
+            var controllerColor = o.Value.Color;
+            var controllerWelcomeString = o.Value.WelcomeString;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Foo()
+        {
+            return View();
+        }
+
+        // GET /HOME/ABOUT
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "I am the home controller.";
+            ViewData["FooMessage"] = "This is custom Foo Message";
+            ViewData["FooCite"] = "http://meessitethis.bitchn";
+
+            var p = new Person()
+            {
+                FirstName = "The First Name",
+                LastName = "The Last Name",
+                Birthday = new DateTime(1909, 1, 23)
+            };
+
+            return View(p);
+        }
+
+        //POST /HOME/ABOUT
+        [HttpPost]
+        public IActionResult About(Person person)
+        {
+
+            return View(person);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MVAMVC/Models/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVAMVC.Models
+{
+    public class Person
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(10, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(10, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Birthday")]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 16)]
+        [DataType(DataType.CreditCard)]
+        [Display(Name = "Credit Card")]
+        public string CreditCard { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public float Amount { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone #")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
+        public string ZipCode { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:H:mm}")]
+        public DateTime Time { get; set; }
+
+        [Required]
+        [DataType(DataType.Upload)]
+        public string Upload { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
+        public string Url { get; set; }
+    }
+}
